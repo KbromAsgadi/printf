23f740b818a8466974447e828f201aa7e5e2dac4
Yeab-Tefera <yeabsra.t123@gmail.com>
2023-03-28 11:58:03 -0900

    error with flags

--- get_flags.c
+++ get_flags.c
@@ -3,17 +3,17 @@
 /**
  * get_flags - A functions that calculates Flags that are active
  * @format: prints arguments that are formmated strings
- * @C: Gets the parameter passed.
+ * @i: Gets the parameter passed.
  * Return: Flags
  */
-int get_flags(const char *format, int *C)
+int get_flags(const char *format, int *i)
 {
 	int C, Curr_j;
 	int Flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (Curr_j = *C + 1; format[Curr_j] != '\0'; Curr_j++)
+	for (Curr_j = *i + 1; format[Curr_j] != '\0'; Curr_j++)
 	{
 		for (C = 0; FLAGS_CH[C] != '\0'; C++)
 			if (format[Curr_j] == FLAGS_CH[C])
@@ -24,7 +24,7 @@ int get_flags(const char *format, int *C)
 		if (FLAGS_CH[C] == 0)
 			break;
 	}
-	*C = Curr_j - 1;
+	*i = Curr_j - 1;
 
 	return (Flags);
 }
